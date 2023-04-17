9b23ed0f0dafed3db4cfc23490014891f5413752
Walter Barbieri <128515233+uolli90@users.noreply.github.com>
2023-04-17 20:01:19 +0200

    Walter 4

---
+++ BW1/assets/js/welcome.js
@@ -0,0 +1,10 @@
+const input = document.querySelector(".radio");
+const button = document.querySelector(".button");
+
+input.addEventListener("click", () => {
+  if (input.checked) {
+    button.removeAttribute("disabled");
+  } else {
+    button.setAttribute("disabled", true);
+  }
+});
