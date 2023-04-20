143262bcec62ee22ea2884778fd27d37036168f5
sofia-sapienza <sofiasapienza99@libero.it>
2023-04-20 15:09:32 +0200

    adds index - Sofia

--- BW1/assets/js/results.js
+++ BW1/assets/js/results.js
@@ -1,2 +1,9 @@
 let data = sessionStorage.getItem(1);
 console.log(data);
+
+/*PROVA SCHEMA CIAMBELLA*/
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
