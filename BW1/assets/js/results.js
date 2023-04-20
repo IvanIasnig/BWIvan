ef20aa195c5d9343d9b6ceaa4986bcf9a1b1eac4
damianocherubini <dami.cher@gmail.com>
2023-04-20 21:43:35 +0200

    DEFINITIVO

--- BW1/assets/js/results.js
+++ BW1/assets/js/results.js
@@ -1,18 +1,31 @@
 let data = sessionStorage.getItem(1);
-let dataWrong = 10 - data;
+let dataWrong = data != null ? 10 - data : 10; // set dataWrong to 0 if data is null
 
 function percentage(partialValue, totalValue) {
-    return (partialValue / totalValue) * 100
-}
-
-function percentageWrong(partialValue, totalValue) {
+    return (partialValue / totalValue) * 100;
+  }
+  
+  function percentageWrong(partialValue, totalValue) {
     return [(totalValue - partialValue) / totalValue] * 100;
-}
-document.getElementById("percetualeCorrect").innerHTML = percentage(data, 10) + "%";
-document.getElementById("quantitaCorrect").innerHTML = data;
-document.getElementById("percetualeWrong").innerHTML = percentageWrong(data, 10) + "%";
-document.getElementById("quantitaWrong").innerHTML = dataWrong;
-
+  }
+  document.getElementById("percetualeCorrect").innerHTML =
+    percentage(data, 10) + "%";
+  document.getElementById("quantitaCorrect").innerHTML = data;
+  document.getElementById("percetualeWrong").innerHTML =
+    percentageWrong(data, 10) + "%";
+  document.getElementById("quantitaWrong").innerHTML = dataWrong;
+  
+  let angolo = dataWrong * 36;
+  let torta = document.querySelector(".torta");
+  torta.style.background = `conic-gradient(#D20094 ${angolo}deg, #00ffff ${angolo}deg)`;
+  
+  function percentageWrong(partialValue, totalValue) {
+      return [(totalValue - partialValue) / totalValue] * 100;
+  }
+  document.getElementById("percetualeCorrect").innerHTML = percentage(data, 10) + "%";
+  document.getElementById("quantitaCorrect").innerHTML = data;
+  document.getElementById("percetualeWrong").innerHTML = percentageWrong(data, 10) + "%";
+  document.getElementById("quantitaWrong").innerHTML = dataWrong;
 
 let result = document.querySelector('#testoTorta');
 function printResult(partialValue) {
@@ -24,7 +37,7 @@ function printResult(partialValue) {
         let cong4 = document.createTextNode('You passed the exam');
         cong3.appendChild(cong4);
         let newNode = document.createElement("p")
-        let cong5 = document.createTextNode('We\'ll send you the certificate in few minutes. Check your email (including promotions / spam folders');
+        let cong5 = document.createTextNode("We\'ll send you the certificate in few minutes. Check your email (including promotions / spam folders)");
         newNode.appendChild(cong5)
         cong3.appendChild(newNode);
         cong1.appendChild(cong3);
@@ -41,7 +54,7 @@ function printResult(partialValue) {
         let cong4 = document.createTextNode('You DIDN\'T pass the exam');
         cong3.appendChild(cong4);
         let newNode = document.createElement("p")
-        let cong5 = document.createTextNode('We ll send your death certificate in few minutes. Check your email (including promotions / spam folders');
+        let cong5 = document.createTextNode('We\'ll send your death certificate in few minutes. Check your email (including promotions / spam folders)');
         newNode.appendChild(cong5)
         cong3.appendChild(newNode);
         cong1.appendChild(cong3);
