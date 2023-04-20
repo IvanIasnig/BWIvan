9a1df34fbdd05030f80c2b86fc8d33de80a8d928
Walter Barbieri <128515233+uolli90@users.noreply.github.com>
2023-04-20 16:14:34 +0200

    WalterDay4Commit5

--- BW1/assets/js/results.js
+++ BW1/assets/js/results.js
@@ -1,2 +1,14 @@
 let data = sessionStorage.getItem(1);
-console.log(data);
+let dataWrong = 10 - data;
+
+function percentage(partialValue, totalValue) {
+    return (partialValue / totalValue) * 100
+}
+
+function percentageWrong(partialValue, totalValue) {
+    return [(totalValue - partialValue) / totalValue] * 100;
+}
+document.getElementById("percetualeCorrect").innerHTML = percentage(data, 10) + "%";
+document.getElementById("quantitaCorrect").innerHTML = data;
+document.getElementById("percetualeWrong").innerHTML = percentageWrong(data, 10) + "%";
+document.getElementById("quantitaWrong").innerHTML = dataWrong;
