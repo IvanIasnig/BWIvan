86f76e412bacc3c5c321abe91d3a8730fc6ad546
sofia-sapienza <sofiasapienza99@libero.it>
2023-04-20 16:24:32 +0200

    adds index

--- BW1/assets/js/results.js
+++ BW1/assets/js/results.js
@@ -18,10 +18,3 @@ document.getElementById("quantitaWrong").innerHTML = dataWrong;
 let angolo = dataWrong * 36;
 let torta = document.querySelector(".torta");
 torta.style.background = `conic-gradient(#D20094 ${angolo}deg, #00ffff ${angolo}deg)`;
-
-/*PROVA SCHEMA CIAMBELLA*/
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
