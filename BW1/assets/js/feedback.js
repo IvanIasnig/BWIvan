b9d9d0f638f99b4c85b6d0c836a6792cb6228077
sofia-sapienza <sofiasapienza99@libero.it>
2023-04-20 15:16:12 +0200

    canvas - Sofia

--- BW1/assets/js/feedback.js
+++ BW1/assets/js/feedback.js
@@ -2,9 +2,10 @@ var rating = 0;
 var stars = document.querySelectorAll("#rating .star");
 
 for (var i = 0; i < stars.length; i++) {
-  stars[i].addEventListener("click", function() {
+  stars[i].addEventListener("click", function () {
     rating = this.getAttribute("data-rating");
     highlightStars();
+    return;
   });
 }
 
@@ -16,4 +17,38 @@ function highlightStars() {
       stars[i].src = "./assets/img/blackStar.svg";
     }
   }
-} 
+}
+function endTest() {
+  alert("Complimenti hai completato il test!")
+}
+
+/**Cicli e funzioni sotto servono a dare effetto hover con mouseover e rimuoverlo con mouseout, ma non riusciamo a disabilitarli al click quindi sono negativi per la user experience
+for (var i = 0; i < stars.length; i++) {
+  stars[i].addEventListener("mouseover", function () {
+    rating = this.getAttribute("data-rating");
+    highlightStars();
+  });
+}
+
+for (var i = 0; i < stars.length; i++) {
+  stars[i].addEventListener("mouseout", function () {
+    rating = this.getAttribute("data-rating");
+    turnoffStar();
+  })
+}
+
+function turnoffStar() {
+  for (var i = 0; i < stars.length; i++) {
+    if (i < rating) {
+      stars[i].src = "./assets/img/blackStar.svg";
+    }
+  }
+}
+
+function lockStar() {
+  for (var i = 0; i < stars.length; i++) {
+    if (i < rating) {
+      stars[i].removeEventListener("mouseout", turnoffStar);
+    }
+}};
+*/
